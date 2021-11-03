2fb05f42af3f3cce3196307b957051eaefc49a89
Adhiraj Ghosh <adhirajghosh1998@gmail.com>
2021-11-03 15:22:46 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,149 @@
+import torch
+from sklearn.metrics import roc_auc_score
+import torch.optim as optim
+import argparse
+import utils
+from tqdm import tqdm
+import torch.nn.functional as F
+import torch.nn as nn
+import wandb
+
+def contrastive_loss(out_1, out_2):
+    out_1 = F.normalize(out_1, dim=-1)
+    out_2 = F.normalize(out_2, dim=-1)
+    bs = out_1.size(0)
+    temp = 0.25
+    # [2*B, D]
+    out = torch.cat([out_1, out_2], dim=0)
+    # [2*B, 2*B]
+    sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / temp)
+    mask = (torch.ones_like(sim_matrix) - torch.eye(2 * bs, device=sim_matrix.device)).bool()
+    # [2B, 2B-1]
+    sim_matrix = sim_matrix.masked_select(mask).view(2 * bs, -1)
+
+    # compute loss
+    pos_sim = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temp)
+    # [2*B]
+    pos_sim = torch.cat([pos_sim, pos_sim], dim=0)
+    loss = (- torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
+    return loss
+
+
+def train_model(model, train_loader, test_loader, train_loader_1, device, args):
+    model.eval()
+    auc, feature_space = get_score(model, device, train_loader, test_loader)
+    print('Epoch: {}, AUROC is: {}'.format(0, auc))
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=0.00005)
+    center = torch.FloatTensor(feature_space).mean(dim=0)
+    center = F.normalize(center, dim=-1)
+    center = center.to(device)
+    loss = nn.CrossEntropyLoss()
+    for epoch in range(args.epochs):
+        running_loss = run_epoch(model, train_loader_1, optimizer, center, device, loss)
+        print('Epoch: {}, Loss: {}'.format(epoch + 1, running_loss))
+        auc, _ = get_score(model, device, train_loader, test_loader)
+        print('Epoch: {}, AUROC is: {}'.format(epoch + 1, auc))
+        # wandb.log({
+        #     "Loss ": running_loss,
+        # })
+
+    wandb.log({
+        "DL AUROC STL 10": auc,
+    })
+
+def run_epoch(model, train_loader, optimizer, center, device, loss_fn):
+    total_loss, total_num = 0.0, 0
+    for ((img1, img2), _) in tqdm(train_loader, desc='Train...'):
+
+        img1, img2 = img1.to(device), img2.to(device)
+
+        optimizer.zero_grad()
+
+        out_1 = model(img1)
+        out_2 = model(img2)
+        out_1 = out_1 - center
+        out_2 = out_2 - center
+
+        center_loss = ((out_1 ** 2).sum(dim=1).mean() + (out_2 ** 2).sum(dim=1).mean())
+        loss = contrastive_loss(out_1, out_2) + center_loss
+
+        loss.backward()
+
+        optimizer.step()
+
+        total_num += img1.size(0)
+        total_loss += loss.item() * img1.size(0)
+    # for ((img1, img2), labels) in tqdm(train_loader, desc='Train...'):
+    #
+    #     img1 = img1.to(device)
+    #     labels = labels.to(device)
+    #     optimizer.zero_grad()
+    #
+    #     out_1 = model(img1)
+    #
+    #     out_1 = out_1 - center
+    #     center_loss = (out_1 ** 2).sum(dim=1).mean()
+    #     loss_val = loss_fn(out_1, labels) + center_loss
+    #
+    #     loss_val.backward()
+    #
+    #     optimizer.step()
+    #
+    #     total_num += img1.size(0)
+    #     total_loss += loss_val.item() * img1.size(0)
+
+    return total_loss / (total_num)
+
+
+def get_score(model, device, train_loader, test_loader):
+    train_feature_space = []
+    with torch.no_grad():
+        for (imgs, _) in tqdm(train_loader, desc='Train set feature extracting'):
+            imgs = imgs.to(device)
+            features = model(imgs)
+            train_feature_space.append(features)
+        train_feature_space = torch.cat(train_feature_space, dim=0).contiguous().cpu().numpy()
+    test_feature_space = []
+    test_labels = []
+    with torch.no_grad():
+        for (imgs, labels) in tqdm(test_loader, desc='Test set feature extracting'):
+            imgs = imgs.to(device)
+            features = model(imgs)
+            test_feature_space.append(features)
+            test_labels.append(labels)
+        test_feature_space = torch.cat(test_feature_space, dim=0).contiguous().cpu().numpy()
+        test_labels = torch.cat(test_labels, dim=0).cpu().numpy()
+
+    distances = utils.knn_score(train_feature_space, test_feature_space)
+
+    auc = roc_auc_score(test_labels, distances)
+
+    return auc, train_feature_space
+
+def main(args):
+    wandb.init(project="DL-AUCROC", entity="adhirajghosh")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+    print("Using model", args.model)
+
+
+    for i in range(10):
+        model = utils.Model(args.model)
+        model = nn.DataParallel(model)
+        model = model.to(device)
+        print('Dataset: {}, Normal Label: {}, LR: {}'.format(args.dataset, i, args.lr))
+        train_loader, test_loader, train_loader_1 = utils.get_loaders(dataset=args.dataset, label_class=i, batch_size=args.batch_size)
+        train_model(model, train_loader, test_loader, train_loader_1, device, args)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--dataset', default='stl10')
+    parser.add_argument('--epochs', default=30, type=int, metavar='epochs', help='number of epochs')
+    parser.add_argument('--label', default=0, type=int, help='The normal class')
+    parser.add_argument('--lr', type=float, default=1e-5, help='The initial learning rate.')
+    parser.add_argument('--batch_size', default=64, type=int)
+    parser.add_argument('--model', default='resnet50')
+    args = parser.parse_args()
+    main(args)
